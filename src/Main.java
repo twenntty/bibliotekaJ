996933df793a4e3c28057e1563f87c2c9953d25d
Mark Bilous <belousme601@gmail.com>
2023-09-22 11:26:51 +0300

    add class Book

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 import java.util.ArrayList;
 import  java.util.List;
 
+class Book {
+    private string title;
+    private string author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
+
+public String  getTitle(){
+    return title;
+}
+
+public String getAuthor(){
+    return author;
+}
+
+public int getYear(){
+    return year;
+}
+
 public class Main {
     public static void main(String[] args)
+
     }
